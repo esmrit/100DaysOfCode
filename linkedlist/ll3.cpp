4cc25aee08496e8f1ec984de56c3836e77573f33
ESMRIT CHANDRA DWIVEDI <89779455+esmrit@users.noreply.github.com>
2022-07-24 00:22:42 +0530

    Add files via upload

---
+++ linkedlist/ll3.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+#include <stdlib.h>
+#include "node1.cpp"
+
+node* takeinput(){
+    int data;
+    cin>> data;
+    node *head=NULL;
+    node *tail=NULL;
+    while (data!=-1){
+        node *newnode=new node(data);
+        if(head == NULL){
+            head =newnode;
+            tail=newnode;
+        }
+        else{
+                
+                tail->next=newnode;
+                tail=tail->next;    
+            }
+    cin>>data;
+                    }
+    return head;    
+    }
+
+
+void print(node *head){
+
+    while (head !=NULL){
+        cout << head->data<<" ";
+        head= head->next;
+    }
+
+}
+
+int main(){
+    node *head=takeinput();
+    print (head);
+}
