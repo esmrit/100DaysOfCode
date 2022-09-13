c989a1b2b9c57fd12c1547a54c6da90de2199b82
ESMRIT CHANDRA DWIVEDI <89779455+esmrit@users.noreply.github.com>
2022-09-13 23:15:34 +0530

    day53

---
+++ revision/numberofnodes.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include "treenode.h"
+#include <queue>
+using namespace std;
+
+
+int noOfNodes(TreeNode<int>* root){
+    if (root->children.size()==0){
+        return 1;
+    }
+
+    int ans=0;
+    for (int  i = 0; i <root->children.size(); i++)
+    {
+        ans=ans+noOfNodes(root->children[i]);
+    }
+    return ans;
+    
+}
+
+TreeNode<int >* takeInputLevelWise(){
+    int rootData;
+    cout<<"enter root daat"<<endl;
+
+    cin>>rootData;
+
+    TreeNode<int >* root=new TreeNode<int >(rootData);
+    queue<TreeNode <int>*> pendingNodes;
+    pendingNodes.push(root);
+
+    while (pendingNodes.size()!=0)
+    {
+        TreeNode<int >*front=pendingNodes.front();
+        pendingNodes.pop();
+        cout<<"enter num of children "<<front->data<<endl;
+        int numChild;
+        cin>>numChild;
+        for (int i = 0; i < numChild; i++)
+        {
+            int childData;
+            cout<<"enter "<<i<<"th child of "<<front->data<<endl;
+            cin>>childData;
+            TreeNode<int>* child= new TreeNode<int>(childData);
+            front->children.push_back(child);
+            pendingNodes.push(child);
+        }
+    }
+    return root;
+}
+
+
+void printTreelevelWise(TreeNode<int>* root){
+    queue <TreeNode<int>*> pn;
+    pn.push(root);
+
+    while(!pn.empty()){
+        TreeNode <int>* cur=pn.front();
+        pn.pop();
+        
+        cout<<cur->data<<":";
+        
+        for(int i=0;i<cur->children.size();i++){
+            if(i==cur->children.size()-1)
+                cout<<cur->children[i]->data;
+            else
+                cout<<cur->children[i]->data<<",";
+            pn.push(cur->children[i]);
+        }
+        cout<<endl;
+    }
+}
+
+
+int main(){
+
+    
+    TreeNode<int >*root= takeInputLevelWise();
+    // printTreelevelWise(root);
+    int answer=noOfNodes(root);
+
+    cout<<answer;
+    
+}
