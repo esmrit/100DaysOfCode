4eb0abf86c0847efce25d264d177f1272edc7f8e
ESMRIT CHANDRA DWIVEDI <89779455+esmrit@users.noreply.github.com>
2022-10-18 21:17:11 +0530

    Day87

---
+++ hieghtOfTree.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+#include "binaryTreeNode.h"
+#include<queue>
+using namespace std;
+
+int heightOfTree(BinaryTreeNode<int>* root){
+    if (root==NULL){
+        return 0;
+    }
+
+    // return 1 + max(heightOfTree(root->left), heightOfTree(root->right));
+}
+
+
+BinaryTreeNode<int>* takeInputLevel(){
+    
+    int rootData;
+    cout<<"enter root data"<<endl;
+    cin >>rootData;
+
+    if (rootData==-1){
+        return NULL;
+    }
+    BinaryTreeNode<int>* root=new BinaryTreeNode<int>(rootData);
+
+    queue <BinaryTreeNode<int>* > pendingnodes;
+    pendingnodes.push(root);
+    while(pendingnodes.size()!=0){
+
+        BinaryTreeNode<int>* front=pendingnodes.front();
+        pendingnodes.pop();
+        cout<<"enter leftchildof "<<front->data<<endl;
+        int leftChildData;
+        cin>>leftChildData;
+    if (leftChildData!=-1){
+        BinaryTreeNode<int>* child=new BinaryTreeNode<int>(leftChildData);\
+        front->left=child;
+        pendingnodes.push(child);
+        }
+       
+       cout<<"enter rightchildof "<<front->data<<endl;
+        int rightChildData;
+        cin>>rightChildData;
+    if (rightChildData!=-1){
+        BinaryTreeNode<int>* child=new BinaryTreeNode<int>(rightChildData);
+        front->right=child;
+        pendingnodes.push(child);
+        }
+    }
+    return root;
+}
+
+int main(){
+
+    BinaryTreeNode <int >*  root=takeInputLevel();
+  
+   int ans =heightOfTree(root); 
+   
+    cout << "hieght "<<ans;
+
+
+}
