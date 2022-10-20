051daa86484ec155b61d3f41c6de83c94a489279
ESMRIT CHANDRA DWIVEDI <89779455+esmrit@users.noreply.github.com>
2022-10-20 18:53:04 +0530

    day90

---
+++ printoutptLEVEL.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+#include "binaryTreeNode.h"
+#include<queue>
+using namespace std;
+
+void printTreeLevel(BinaryTreeNode<int>* root){
+    if (root==NULL){
+        return;
+    }
+    queue <BinaryTreeNode<int>* > qe;
+    qe.push(root);
+
+    while(qe.size()!=0){
+        BinaryTreeNode<int>* front=qe.front();
+        cout<<front->data<<" ";
+        qe.pop();
+
+        if (front->left!=NULL){
+            qe.push(front->left);
+        }
+        
+        if (front->right!=NULL){
+            qe.push(front->right);
+        }
+    }
+}
+
+BinaryTreeNode<int>* takeInputLevel(){
+    
+    int rootData;
+    cout<<"enter root data"<<endl;
+    cin >>rootData;
+
+    if (rootData==-1){
+        return NULL;
+    }
+    BinaryTreeNode<int>* root=new BinaryTreeNode<int>(rootData);
+
+    queue <BinaryTreeNode<int>* > pendingnodes;
+    pendingnodes.push(root);
+    while(pendingnodes.size()!=0){
+
+        BinaryTreeNode<int>* front=pendingnodes.front();
+        pendingnodes.pop();
+        cout<<"enter leftchildof "<<front->data<<endl;
+        int leftChildData;
+        cin>>leftChildData;
+    if (leftChildData!=-1){
+        BinaryTreeNode<int>* child=new BinaryTreeNode<int>(leftChildData);\
+        front->left=child;
+        pendingnodes.push(child);
+        }
+       
+       cout<<"enter rightchildof "<<front->data<<endl;
+        int rightChildData;
+        cin>>rightChildData;
+    if (rightChildData!=-1){
+        BinaryTreeNode<int>* child=new BinaryTreeNode<int>(rightChildData);
+        front->right=child;
+        pendingnodes.push(child);
+        }
+    }
+    return root;
+}
+
+int main(){
+    // BinaryTreeNode<int>* root=new BinaryTreeNode<int>(1);
+    // BinaryTreeNode<int>* node1=new BinaryTreeNode<int>(2);
+    // BinaryTreeNode<int>* node2=new BinaryTreeNode<int>(3);
+BinaryTreeNode<int>* root=takeInputLevel();
+    // root->left=node1;
+    // root->right=node2;
+    
+    printTreeLevel(root);
+}
